4d7ef2b5f5b1bd8ee5f49045e38274215fbb2ffd
Adarsh Dubey <adarshdubey8889@gmail.com>
2026-05-26 11:26:39 +0530

    Fixed static root

--- config/settings.py
+++ config/settings.py
@@ -146,3 +146,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
